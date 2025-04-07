ac5c44efe9d7dcf9cef8d2d5445d062229e1d377
Kevin-Garri <kevin.alexander.garri29@gmail.com>
2025-04-07 11:57:06 +0200

    add: snack2

--- Snack1.js
+++ Snack1.js
@@ -14,3 +14,8 @@ const somma = function (num1, num2) {
 //arrow function
 const somma = (num1, num2) => num1 + num2;
 
+
+/*Snack 2 Crea una arrow function che calcola il quadrato di un numero.
+Definisci una funzione chiamata quadrato che accetta un numero e restituisce il suo quadrato in una sola riga.
+*/
+const quadrato = numero => numero * numero;
